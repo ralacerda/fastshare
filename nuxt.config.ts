35cbda2ec31cf6b87a05e0fcc529311e6dc7263e
Renato Lacerda <renato.ac.lacerda@gmail.com>
2024-09-03 19:33:27 -0300

    chore: page title and lang attribute

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,10 @@ export default defineNuxtConfig({
 
   app: {
     head: {
+      title: "Fast Share",
+      htmlAttrs: {
+        lang: "en",
+      },
       link: [
         {
           href: "https://cdn.jsdelivr.net/npm/bulma@1.0.1/css/bulma.min.css",
